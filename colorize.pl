7e599567a81c55b45fd5b858bfaed360e84a8a3d
Andrej Kvasnica <andrej@gmail.com>
2014-03-05 11:44:41 +0100

    added link to github repo, removed trailing spaces

--- colorize.pl
+++ colorize.pl
@@ -35,7 +35,7 @@ my @colorpresets = ("u18", "u28", "u38", "u48", "u78", "l10", "l20", "l30", "l40
 # main 
 #
 if ($argscount == -1) { 
-	print "Error: No options entered ! \n" ; 
+	print "Error: No options entered !\n" ; 
 	about() ; 
 }
 
@@ -84,27 +84,28 @@ exit 0 ;
 #
 
 sub about () {
-	print "colorize, The stdin-stdout line colorizer, version:$VERSION by daniel flinkmann, www.flinkmann.de \n"; 
+	print "colorize, The stdin-stdout line colorizer, version:$VERSION by daniel flinkmann, www.flinkmann.de (defunct)\n"; 
+	print "currently available at https://github.com/4ndrej/colorize public repo\n"; 
 	print "\nUsage:\t" ;
 	print "Colorize with matching string:\n";
 	print "$0 [-+]<style><foreground color><background color>:<searchstring>\n\n" ; 
 	print "Quick-Usage:\n" ; 
-	print "$0 -:<searchstring> -:<searchstring> -:<searchstring> \n" ;
-	print "$0 +:<searchstring> +:<searchstring> +:<searchstring> \n\n" ; 
-	print "- will colorize whole matching rows, + will colorize the matched portion only. \n" ;
-	print "<style>            : n = normal, l = light, u=underscore, i = inverted, b = blinking \n" ; 
-	print "<foreground color> : 0 = black, 1 = red, 2 = green, 3 = yellow, 4 = blue, 5 = purple, 6 = cyan, 7 = white \n"; 
-	print "<background color> : 0 = black, 1 = red, 2 = green, 3 = yellow, 4 = blue, 5 = purple, 6 = cyan, 7 = white 8 = no Background \n";
-	print "<searchstring>     : string \n\n" ;
+	print "$0 -:<searchstring> -:<searchstring> -:<searchstring>\n" ;
+	print "$0 +:<searchstring> +:<searchstring> +:<searchstring>\n\n" ; 
+	print "- will colorize whole matching rows, + will colorize the matched portion only.\n" ;
+	print "<style>            : n = normal, l = light, u=underscore, i = inverted, b = blinking\n" ; 
+	print "<foreground color> : 0 = black, 1 = red, 2 = green, 3 = yellow, 4 = blue, 5 = purple, 6 = cyan, 7 = white\n"; 
+	print "<background color> : 0 = black, 1 = red, 2 = green, 3 = yellow, 4 = blue, 5 = purple, 6 = cyan, 7 = white 8 = no Background\n";
+	print "<searchstring>     : string\n\n" ;
 	print "Note: You can enter as much search strings as you like, however the latter can overwrite the earlier\n"; 
 	print "(Searchstrings in +:<searchstring> can be regular expressions, but not in -:<searchstring>)\n\n"; 
 	print "Examples:\n"; 
 	print "Normal colorisation with users chosen colors\n" ; 
-	print "ls -lF / \| $0 -n17:usr -u34:home +l58:etc \n" ;
+	print "ls -lF / \| $0 -n17:usr -u34:home +l58:etc\n" ;
 	print "Prints out the root directory with red/white \"usr\" dir (whole row), underscored yellow/blue \"home\" directory (whole row) and the word etc will be colorized in light purple with no background color.\n\n"; 
-	print "Example of a quick colorisation with preset colors: \n"; 
-	print "ls -lF / \| $0 -:usr +:home \n" ; 
-	print "Prints out the root directory with the first preset color \"usr\" dir (whole row) and second preset  \"home\" directory (only matched word) \n";
+	print "Example of a quick colorisation with preset colors:\n"; 
+	print "ls -lF / \| $0 -:usr +:home\n" ; 
+	print "Prints out the root directory with the first preset color \"usr\" dir (whole row) and second preset  \"home\" directory (only matched word)\n";
 	exit (-1) ; 
 }
 
